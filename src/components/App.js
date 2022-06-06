c7994b80301d9ffc0cae1d4d06b3aad0fb4cc21e
Anton Honcharov <antonhoncharovv@gmail.com>
2022-06-06 16:57:59 +0300

    add localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -7,15 +7,23 @@ import Filter from "./Filter/Filter";
 class App extends Component {
 
   state = {
-    contacts: [
-    {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-    ],
+    contacts: [],
     filter: '',
   }
   
+  componentDidMount() {
+    const contacts = JSON.parse(localStorage.getItem('contacts'))
+    if (contacts) {
+      this.setState({contacts})
+    }
+  }
+
+  componentDidUpdate(PrevProp, PrevState) {
+    if (this.state.contacts !== PrevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts))
+    }
+  }
+
   HandlerSubmitForm = ({name, number}) => {
     const newUser = { id: nanoid(5), name, number };
     const newUserNormalized = name.toLowerCase();
